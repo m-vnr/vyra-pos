b92d2bc7a2a84a33e8a598dfb92925eba1ad79cd
m-vnr <mvnrtdp@gmail.com>
2026-06-20 23:35:37 +0530

    Integrate login with auth service

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,6 +1,20 @@
 import { useState } from 'react';
+import { signIn } from '../services/authService';
 
 export default function LoginPage(){
  const [email,setEmail]=useState('');
- return <div><h1>Vyra POS Login</h1><input value={email} onChange={(e)=>setEmail(e.target.value)} placeholder='Email' /></div>;
+ const [password,setPassword]=useState('');
+
+ const login = async () => {
+   await signIn(email,password);
+ };
+
+ return (
+  <div>
+   <h1>Vyra POS Login</h1>
+   <input value={email} onChange={(e)=>setEmail(e.target.value)} placeholder='Email' />
+   <input type='password' value={password} onChange={(e)=>setPassword(e.target.value)} placeholder='Password' />
+   <button onClick={login}>Login</button>
+  </div>
+ );
 }
