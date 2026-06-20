2da8c0e19873ad81b02592c8efeb426c090cf3e2
m-vnr <mvnrtdp@gmail.com>
2026-06-20 23:32:18 +0530

    Integrate cashier page with POS components

--- src/pages/CashierPage.tsx
+++ src/pages/CashierPage.tsx
@@ -1,11 +1,20 @@
+import ProductGrid from '../components/ProductGrid';
+import CartPanel from '../components/CartPanel';
+import PaymentModal from '../components/PaymentModal';
 import { useCartStore } from '../store/cartStore';
 
 export default function CashierPage(){
  const items = useCartStore((s:any)=>s.items);
+
  return (
   <div>
    <h1>Cashier POS</h1>
    <p>Items in cart: {items.length}</p>
+   <div>
+    <ProductGrid />
+    <CartPanel />
+    <PaymentModal />
+   </div>
   </div>
  );
 }
